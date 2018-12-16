08a3c7f19995764643600d72698ecfe714621392
Cyakat <36944093+Cyakat@users.noreply.github.com>
2018-12-15 17:06:37 -0700

    Updated index.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 function main () {
+    $('.nav-button').on('click', function () {
+        if ($('.nav-button')) {
+            $('.nav-button').toggleClass('click-slide-out');
+            $('.nav-button').toggleClass('click-slide-in');
+            $('.slidable').toggleClass('slide-in');
+            $('.slidable').toggleClass('slide-out');
+            console.log('owo');
+            
+        }
+    });
+    $('.main-page-button').on('click', function () {
+        $('.main-page').siblings().fadeOut();
+        $('.main-page').fadeIn();
+    });
+    $('.button').on('click', function () {
+        $('.downloads-page').removeClass('invis');
+        $('.downloads-page').siblings().removeClass('invis');
+    });
+    $('.downloads-button').on('click', function () {
+        $('.downloads-page').siblings().fadeOut();
+        $('.downloads-page').fadeIn();
 
+    });
+    $('.misc-button').on('click', function () {
+        $('.misc-page').siblings().fadeOut();
+        $('.misc-page').fadeIn();
+    });
+
+    
 }
 $(document).ready(main);
