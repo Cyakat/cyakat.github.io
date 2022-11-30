cea3c9eb89faf64c282c6c232750d2be34cd0c6f
Cyakat <noah@thebauers.net>
2022-11-30 08:51:03 -0700

    Made a front page for the generated webapges

---
+++ web class/final project/js/index.js
@@ -0,0 +1,7 @@
+codeList = ['cyakat.github.io','DiscordCatBot','DiscordOwOBot','guess-who-website','machine-learning','Word-Stats.html','Wordle-bot']
+
+$(document).ready(() => {
+    for (var i = 0; i < codeList.length; i++) {
+        ""
+    }
+});
